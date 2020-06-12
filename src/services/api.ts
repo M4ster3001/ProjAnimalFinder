bf0d20e4110f354aefb57414b01db9718fa0c6b5
M4ster3001 <42715849+M4ster3001@users.noreply.github.com>
2020-06-12 14:42:48 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import Cookies from 'js-cookie'
-require('dotenv').config({ path: '.env' })
-console.log( process.env.REACT_APP_API_URl )
+
 //const token = Buffer.from(`${Cookies.get( 'token' )}`).toString('base64')
 const token = Cookies.get( 'token' );
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URl,
+    baseURL: 'https://ong-an-finders-backend.herokuapp.com/',
     timeout: 30000,
     withCredentials: true,
     headers: {
@@ -16,7 +15,7 @@ export const api = axios.create({
 })
 
 export const apiLogin = axios.create({
-    baseURL: process.env.REACT_APP_API_URl,
+    baseURL: 'https://ong-an-finders-backend.herokuapp.com/',
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json; charset=utf-8',
